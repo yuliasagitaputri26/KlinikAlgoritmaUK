f2cf0364f243c9156672fb4d4da902259d9ca278
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2025-11-16 20:08:28 +0800

    Create Array2_No2.java
    This program reads an integer array from user input and finds the second largest number.

---
+++ Array2_No2.java
@@ -0,0 +1,35 @@
+package pertemuan4;
+
+import java.util.Scanner;
+
+public class Soal2 {
+
+    public static void main(String[] args) {
+
+        Scanner yu = new Scanner(System.in);
+        int n = yu.nextInt();
+
+        int[] data = new int[n];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = yu.nextInt();
+        }
+
+        int terbesar = Integer.MIN_VALUE;
+        int terbesarKedua = Integer.MIN_VALUE;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > terbesar) {
+                terbesar = data[i];
+            }
+        }
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > terbesarKedua && data[i] < terbesar) {
+                terbesarKedua = data[i];
+            }
+        }
+
+        System.out.println(terbesarKedua);
+
+    }
+}
