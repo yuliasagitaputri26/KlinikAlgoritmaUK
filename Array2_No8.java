6c8d870bd5d1fa766dd40137b077b8695381c9f2
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2025-11-16 20:29:12 +0800

    Create Array2_No8.java

---
+++ Array2_No8.java
@@ -0,0 +1,31 @@
+package pertemuan4;
+
+import java.util.Scanner;
+
+public class Soal8 {
+
+    public static void main(String[] args) {
+
+        Scanner yu = new Scanner(System.in);
+        int n = yu.nextInt();
+
+        int A[] = new int[n];
+        int B[] = new int[n];
+        int C[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            A[i] = yu.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            B[i] = yu.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            C[i] = A[i] + B[i];
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(C[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
